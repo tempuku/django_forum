1762e5b95deac98c8b4e52c4ba440f0b5a0be6f1
Alexandr <volkov.alexandr.pavl@gmail.com>
2019-08-29 19:45:34 +0300

    Add class 'Comment'

--- forum/models.py
+++ forum/models.py
@@ -5,3 +5,10 @@ class Forum(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     title = models.CharField(max_length=100)
     created_at = models.DateTimeField(auto_now_add=True)
+
+
+class Comment(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    forum = models.ForeignKey(Forum, on_delete=models.CASCADE)
+    desc = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
