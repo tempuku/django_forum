436262699e7925d0429c688e92e14a56ee8069b2
Alexandr <volkov.alexandr.pavl@gmail.com>
2019-08-29 12:53:14 +0300

    Add model /Forum/

---
+++ forum/models.py
@@ -0,0 +1,7 @@
+from django.db import models
+from django.conf import settings
+
+class Forum(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    title = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
