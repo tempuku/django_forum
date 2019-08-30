d792b253ed746aabbe2bc98900f5c501e5cf1822
Alexandr <volkov.alexandr.pavl@gmail.com>
2019-08-30 16:56:10 +0300

    Create views 'forum', 'index', 'comment'.

--- forum/models.py
+++ forum/models.py
@@ -2,13 +2,13 @@ from django.db import models
 from django.conf import settings
 
 class Forum(models.Model):
-    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL)
     title = models.CharField(max_length=100)
     created_at = models.DateTimeField(auto_now_add=True)
 
 
 class Comment(models.Model):
-    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    forum = models.ForeignKey(Forum, on_delete=models.CASCADE)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL)
+    forum = models.ForeignKey(Forum, on_delete=models.CASCADE, related_name="comments")
     desc = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
--- forum/views.py
+++ forum/views.py
@@ -1,3 +1,28 @@
 from django.shortcuts import render
+from .models import Forum, Comment
 
 # Create your views here.
+def index(request):
+    context = {
+        "forums": Forum.object.all()
+    }
+    return render(request, "forum/index.html", context)
+
+def forum(request, forum_id):
+    try:
+        forum = Forum.objects.get(pk=forum_id)
+    except: Forum.DoesNotExist:
+        raise Http404("Forum does not exist")
+    context = {
+    "forum": forum,
+    }
+    return render(request, "forum/forum.html", context)
+
+def comment(request, forum_id):
+    if request.method != 'POST':
+        raise Http404('Only POSTs are allowed')
+    comment_text = request.POST["comment"]
+    f = Forum.object.get(pk=forum_id)
+    c = Comment(forum=f, desc=comment_text)
+    c.save()
+    return HttpResponseRedirect(reverse("forum", args=(forum_id)))
